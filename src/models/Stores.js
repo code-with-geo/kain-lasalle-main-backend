d0601005760009e3b1eea9df597485aa8cdf627f
Geo Xyruz Ranas <g.x.ranas@gmail.com>
2024-05-23 14:14:57 +0800

    add store

--- src/models/Stores.js
+++ src/models/Stores.js
@@ -2,8 +2,16 @@ import mongoose from "mongoose";
 
 const StoreSchema = mongoose.Schema({
 	name: { type: String, required: true, unique: true },
+	address: { type: String, required: true },
 	description: { type: String, required: true },
+	contactperson: { type: String, required: true },
+	contactno: { type: String, required: true },
+	storehour: { type: String, required: true },
 	image: { type: String, default: "" },
+	createAt: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 StoreSchema.virtual("id").get(function () {
